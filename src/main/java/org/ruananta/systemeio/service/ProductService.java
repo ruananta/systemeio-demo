16ff0c74f351d0c77b592178d9d85eb7d890a663
ruananta <ananta69g@gmail.com>
2024-06-10 13:43:42 +0545

    add default product to db

--- src/main/java/org/ruananta/systemeio/service/ProductService.java
+++ src/main/java/org/ruananta/systemeio/service/ProductService.java
@@ -2,8 +2,11 @@ package org.ruananta.systemeio.service;
 
 import org.ruananta.systemeio.entity.Product;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductService {
     public Optional<Product> findById(long id);
+    public List<Product> findAll();
+    boolean existsById(long id);
 }
--- src/main/java/org/ruananta/systemeio/service/ProductServiceImpl.java
+++ src/main/java/org/ruananta/systemeio/service/ProductServiceImpl.java
@@ -1,10 +1,14 @@
 package org.ruananta.systemeio.service;
 
+import jakarta.annotation.PostConstruct;
 import org.ruananta.systemeio.entity.Product;
 import org.ruananta.systemeio.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 @Component
@@ -17,8 +21,31 @@ public class ProductServiceImpl implements ProductService {
         this.productRepository = productRepository;
     }
 
+    @PostConstruct
+    public void initDB() {
+        if (productRepository.count() == 0) {
+            List<Product> products = Arrays.asList(
+                    new Product("Iphone", "Описание Iphone", new BigDecimal("100.00")),
+                    new Product("Наушники", "Описание наушников", new BigDecimal("20.0")),
+                    new Product("Чехол", "Описание чехла", new BigDecimal("10.0"))
+            );
+            productRepository.saveAll(products);
+        }
+    }
+
     @Override
     public Optional<Product> findById(long id) {
         return this.productRepository.findById(id);
     }
+
+    @Override
+    public List<Product> findAll() {
+        return this.productRepository.findAll();
+    }
+
+    @Override
+    public boolean existsById(long id) {
+        return this.productRepository.existsById(id);
+    }
+
 }
