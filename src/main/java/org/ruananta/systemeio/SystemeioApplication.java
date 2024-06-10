0735c6ad69062a59a32903a83f66533d29729c2d
ruananta <ananta69g@gmail.com>
2024-06-10 10:36:09 +0545

    create project

---
+++ src/main/java/org/ruananta/systemeio/SystemeioApplication.java
@@ -0,0 +1,13 @@
+package org.ruananta.systemeio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemeioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemeioApplication.class, args);
+	}
+
+}
