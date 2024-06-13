2b0b88c966d506f974e386728ed9ce817eaf3598
ruananta <ananta69g@gmail.com>
2024-06-13 08:10:47 +0545

    add test to fixed coupon for tax service

--- src/test/java/org/ruananta/systemeio/service/TaxServiceTest.java
+++ src/test/java/org/ruananta/systemeio/service/TaxServiceTest.java
@@ -54,6 +54,31 @@ public class TaxServiceTest {
     }
 
     @Test
+    public void calculateFinalPrice_WithValidCoupon_ShouldApplyFixedDiscount() {
+        Long productId = 753L;
+        String taxNumber = "GR123456789";
+        String couponCode = "F20";
+        BigDecimal basePrice = new BigDecimal("110.00");
+        BigDecimal discount = new BigDecimal("10.00");
+        String country = "GREECE";
+        BigDecimal taxRate = new BigDecimal("0.24");
+        BigDecimal finalPrice = new BigDecimal("124.00");
+
+        CalculatePriceRequest request = new CalculatePriceRequest(productId, taxNumber, couponCode);
+        Product product = new Product(productId, basePrice);
+        Coupon coupon = new Coupon(couponCode, DiscountType.FIXED, discount);
+
+        when(productService.findById(productId)).thenReturn(Optional.of(product));
+        when(couponService.getCoupon(couponCode)).thenReturn(Optional.of(coupon));
+        when(taxConfiguration.getCountryFromTaxNumber(taxNumber)).thenReturn(country);
+        when(taxConfiguration.getTaxRateFromCountry(country)).thenReturn(taxRate);
+
+        BigDecimal calculateFinalPrice = taxService.calculateFinalPrice(request);
+
+        assertEquals("Final price should be discounted", finalPrice, calculateFinalPrice);
+    }
+
+    @Test
     public void calculateFinalPrice_WithInvalidCoupon_ShouldNotApplyDiscount() {
         Long productId = 753L;
         String taxNumber = "GR123456789";
