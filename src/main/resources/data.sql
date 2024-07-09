04600eb1743e4181bac40d245065c36d8ea27a40
ruananta <ananta69g@gmail.com>
2024-07-09 14:11:24 +0545

    update data.sql initialization, added data check in DB

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,24 @@
+-- Вставка данных в таблицу product, если она пустая
+INSERT INTO product (description, name, price)
+SELECT 'Описание Iphone', 'Iphone', 100.00
+WHERE NOT EXISTS (SELECT 1 FROM product)
+UNION ALL
+SELECT 'Описание наушников', 'Наушники', 20.00
+WHERE NOT EXISTS (SELECT 1 FROM product)
+UNION ALL
+SELECT 'Описание чехла', 'Чехол', 10.00
+WHERE NOT EXISTS (SELECT 1 FROM product);
 
--- Вставка данных в таблицу product
-INSERT INTO product (description, name, price) VALUES ('Описание Iphone', 'Iphone', 100.00);
-INSERT INTO product (description, name, price) VALUES ('Описание наушников', 'Наушники', 20.00);
-INSERT INTO product (description, name, price) VALUES ('Описание чехла', 'Чехол', 10.00);
-
--- Вставка данных в таблицу coupon
-INSERT INTO coupon (code, discount_type, discount) VALUES ('P6', 0, 6.00);
-INSERT INTO coupon (code, discount_type, discount) VALUES ('P10', 0, 10.00);
-INSERT INTO coupon (code, discount_type, discount) VALUES ('P100', 0, 100.00);
-INSERT INTO coupon (code, discount_type, discount) VALUES ('F10', 1, 10.00);
+-- Вставка данных в таблицу coupon, если она пустая
+INSERT INTO coupon (code, discount_type, discount)
+SELECT 'P6', 0, 6.00
+WHERE NOT EXISTS (SELECT 1 FROM coupon)
+UNION ALL
+SELECT 'P10', 0, 10.00
+WHERE NOT EXISTS (SELECT 1 FROM coupon)
+UNION ALL
+SELECT 'P100', 0, 100.00
+WHERE NOT EXISTS (SELECT 1 FROM coupon)
+UNION ALL
+SELECT 'F10', 1, 10.00
+WHERE NOT EXISTS (SELECT 1 FROM coupon);
